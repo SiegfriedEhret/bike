da9c4afc9e7f5b4d758586a88ee5c759a99955da
Vincent De Oliveira <vincent@iamvdo.me>
2013-10-28 14:33:47 +0100

    Modifiy popup content + add icons (see comments)

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -47,8 +47,16 @@
 
     function initMap() {
         map = L.map('map');
+        /*
         L.tileLayer('http://{s}.tile.cloudmade.com/f132755677b94dd7b7d3629ea3ef44f8/997/256/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>[…]',
+            maxZoom: 18
+        }).addTo(map);
+        */
+
+        // a better tile provider
+        L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri',
             maxZoom: 18
         }).addTo(map);
     }
@@ -62,10 +70,27 @@
             var markers = new L.MarkerClusterGroup();
             var markersList = [];
 
+            // icon for markers (maybe not here)
+            var iconMarker = {
+                icon: L.divIcon({
+                    iconSize: [22,22],
+                    popupAnchor: [0,-5],
+                    className: 'leaflet-marker-station'
+                })
+            };
+            
             function populate() {
                 for (var i = 0; i < data.length; i++) {
                     var poi = data[i];
-                    var m = new L.marker([poi.position.lat, poi.position.lng]).bindPopup(poi.address);
+                    // set popup content (maybe not here too)
+                    var txtAddress = '<p>' + poi.name + '</p>';
+                    var iconAdd = '<div class="icon-bike icon-bike--add icon-bike-popup">' + poi.available_bikes + '</div>';
+                    var iconRemove = '<div class="icon-bike icon-bike--remove icon-bike-popup">' + poi.available_bike_stands + '</div>';
+                    var icons = '<div class="icons">' + iconAdd + iconRemove + '</div>';
+                    var txtPopup = txtAddress + icons;
+
+
+                    var m = new L.marker([poi.position.lat, poi.position.lng], iconMarker).bindPopup(txtPopup);
                     markersList.push(m);
                     markers.addLayer(m);
                 }
